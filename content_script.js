c3c3c18fe1c96ea7c91ed984021368beffae8bb6
Vijay Kumar <vijay.kumar2@in.pega.com>
2024-08-31 14:36:39 +0530

    Removes unused code

--- content_script.js
+++ content_script.js
@@ -1,15 +1,15 @@
-document.addEventListener("click", (event) => {
-  chrome.runtime.sendMessage(
-    {
-      click: true,
-      xPosition: event.clientX + document.body.scrollLeft,
-      yPosition: event.clientY + document.body.scrollTop,
-    },
-    (response) => {
-      console.log("Received response", response);
-    }
-  );
-});
+// document.addEventListener("click", (event) => {
+//   chrome.runtime.sendMessage(
+//     {
+//       click: true,
+//       xPosition: event.clientX + document.body.scrollLeft,
+//       yPosition: event.clientY + document.body.scrollTop,
+//     },
+//     (response) => {
+//       console.log("Received response", response);
+//     }
+//   );
+// });
 
 // Listen for messages posted to the window
 window.addEventListener(
