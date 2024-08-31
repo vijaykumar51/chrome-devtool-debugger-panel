a36d5e8033c99b851e5dceaf9cf69f01bb279ec4
Vijay Kumar <vijay.kumar2@in.pega.com>
2024-08-31 14:38:10 +0530

    Removes unused background.js

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  // Send the message to the DevTools panel
-  console.log("background: message", message);
-  chrome.runtime.sendMessage(message);
-});
