78d82a0ef381d3f9ef7520015661060599bde119
Vijay Kumar <vijay.kumar2@in.pega.com>
2024-09-08 01:14:08 +0530

    preparing the nested structure from event

--- tabpanel-ui/src/mock/mockData.js
+++ tabpanel-ui/src/mock/mockData.js
@@ -54,12 +54,23 @@
 //   },
 // };
 
-const getAction = (actionType, actionName, color, indentLevel) => ({
+const getAction = (
+  id,
+  actionType,
+  actionName,
+  color,
+  indentLevel,
+  isNestedLogStart = false,
+  isNestedLogEnd = false
+) => ({
+  id,
   actionType,
   actionInfo: "Action dispatched to the queue",
   actionName,
   color,
   indentLevel,
+  isNestedLogStart,
+  isNestedLogEnd,
   details: {
     payload: {
       action: {
@@ -75,53 +86,171 @@ const getAction = (actionType, actionName, color, indentLevel) => ({
 });
 
 // TODO: check to have another prop "isStandalone" to see if the property is standalone or not. If not, then dispatch another event of same type with "close event"
+// or take prop "willClose" to make sure any new event is nested under current one.
 
-const action1 = getAction("DISPATCH ACTION", "pagination", "#d7658b", 1);
-const action2 = getAction("DISPATCH ACTION", "bootstrap", "#d7658b", 1);
+const action1 = getAction(1, "DISPATCH ACTION", "pagination", "#d7658b", 1);
+const action2 = getAction(2, "DISPATCH ACTION", "bootstrap", "#d7658b", 1);
 const action3 = getAction(
+  3,
   "DISPATCH ACTION",
   "updateStateVersion",
   "#d7658b",
   1
 );
-const action4 = getAction("DISPATCH ACTION", "forceUpdate", "#d7658b", 1);
+const action4 = getAction(4, "DISPATCH ACTION", "forceUpdate", "#d7658b", 1);
 
-const action5 = getAction("EXECUTING ACTION", "pagination", "#76c68f", 1);
+const action5 = getAction(
+  5,
+  "EXECUTING ACTION",
+  "pagination",
+  "#76c68f",
+  1,
+  true
+);
 
-const action6 = getAction("REDUCED STATE", "", "#54bebe", 2);
-const action7 = getAction("EXECUTING FEATURES", "", "#54bebe", 2);
+const action6 = getAction(6, "REDUCED STATE", "", "#54bebe", 2);
+const action7 = getAction(7, "EXECUTING FEATURES", "", "#54bebe", 2, true);
 
-const action8 = getAction("EXECUTING FEATURE", "personalization", "#e76157", 3);
-const action9 = getAction("EXECUTING FEATURE", "filter", "#e76157", 3);
-const action10 = getAction("EXECUTING FEATURE", "grouping", "#e76157", 3);
+const action8 = getAction(
+  8,
+  "EXECUTING FEATURE",
+  "personalization",
+  "#e76157",
+  3
+);
+const action9 = getAction(9, "EXECUTING FEATURE", "filter", "#e76157", 3);
+const action10 = getAction(10, "EXECUTING FEATURE", "grouping", "#e76157", 3);
 
-const action11 = getAction("EXECUTING ACTION", "bootstrap", "#76c68f", 1);
+const action11 = getAction(
+  7,
+  "EXECUTING FEATURES",
+  "",
+  "#54bebe",
+  2,
+  false,
+  true
+);
+const action12 = getAction(
+  5,
+  "EXECUTING ACTION",
+  "pagination",
+  "#76c68f",
+  1,
+  false,
+  true
+);
 
-const action12 = getAction("REDUCED STATE", "", "#54bebe", 2);
-const action13 = getAction("EXECUTING FEATURES", "", "#54bebe", 2);
+const action13 = getAction(
+  11,
+  "EXECUTING ACTION",
+  "bootstrap",
+  "#76c68f",
+  1,
+  true
+);
 
-const action14 = getAction(
+const action14 = getAction(12, "REDUCED STATE", "", "#54bebe", 2);
+const action15 = getAction(13, "EXECUTING FEATURES", "", "#54bebe", 2, true);
+
+const action16 = getAction(
+  14,
   "EXECUTING FEATURE",
   "personalization",
   "#e76157",
   3
 );
 
+const action17 = getAction(
+  13,
+  "EXECUTING FEATURES",
+  "",
+  "#54bebe",
+  2,
+  false,
+  true
+);
+const action18 = getAction(
+  11,
+  "EXECUTING ACTION",
+  "bootstrap",
+  "#76c68f",
+  1,
+  false,
+  true
+);
+
 export const mockData = {
   events: [
-    { id: "1", eventDetails: action1 },
-    { id: "2", eventDetails: action2 },
-    { id: "3", eventDetails: action3 },
-    { id: "4", eventDetails: action4 },
-    { id: "5", eventDetails: action5 },
-    { id: "6", eventDetails: action6 },
-    { id: "7", eventDetails: action7 },
-    { id: "8", eventDetails: action8 },
-    { id: "9", eventDetails: action9 },
-    { id: "10", eventDetails: action10 },
-    { id: "11", eventDetails: action11 },
-    { id: "12", eventDetails: action12 },
-    { id: "13", eventDetails: action13 },
-    { id: "14", eventDetails: action14 },
+    action1,
+    action2,
+    action3,
+    action4,
+    action5,
+    action6,
+    action7,
+    action8,
+    action9,
+    action10,
+    action11,
+    action12,
+    action13,
+    action14,
+    action15,
+    action16,
+    action17,
+    action18,
   ],
 };
+
+// let id = 0;
+// const generateEventId = () => {
+//   id++;
+//   return id;
+// };
+
+const nestedQueue = [];
+const queueBuckedMap = new Map();
+
+const processedEventInfo = [];
+
+const initStateProcessing = () => {
+  let currentEventBucket = processedEventInfo;
+  mockData.events.forEach((e) => {
+    const newEventBucket = handleEvent(e, currentEventBucket);
+    if (newEventBucket) {
+      currentEventBucket = newEventBucket;
+    }
+  });
+};
+
+const handleEvent = (event, currentEventBucket) => {
+  const { isNestedLogStart, isNestedLogEnd, id } = event;
+  const isNestedEvent = isNestedLogStart || isNestedLogEnd;
+
+  if (isNestedEvent) {
+    if (isNestedLogStart) {
+      const nestedEventBucket = [];
+      currentEventBucket.push({ ...event, nestedEventBucket });
+
+      nestedQueue.push(id);
+      queueBuckedMap.set(id, nestedEventBucket);
+      return nestedEventBucket;
+    } else if (isNestedLogEnd) {
+      nestedQueue.pop();
+      queueBuckedMap.delete(id);
+
+      if (nestedQueue.length > 0) {
+        const newBucketId = nestedQueue[nestedQueue.length - 1];
+        return queueBuckedMap.get(newBucketId);
+      } else {
+        return processedEventInfo;
+      }
+    }
+  } else {
+    currentEventBucket.push(event);
+  }
+};
+
+initStateProcessing();
+
+console.log(processedEventInfo);
